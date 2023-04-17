14a15a4e847645a58c9d10ebbe7c7ffffd4a4198
Ryan Lundy <rlundy1792@gmail.com>
2023-04-17 11:18:23 -0400

    Configured barebones express app

---
+++ app.js
@@ -0,0 +1,13 @@
+// here I'll configure express app
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Budget App! For Docs, please visit...")
+})
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,10 @@
+// here I'll env variable to  start my server and other services
+require("dotenv").config();
+
+const app = require("./app");
+
+const PORT = process.env.PORT || 3333
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
